afad48eceb183cb968aaaf6c666fdc56ece448b8
oxfeeefeee <pb01005051@gmail.com>
2022-05-04 15:42:03 +0800

    fixed in and out

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,13 @@ use std::io::prelude::*;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-extern "C" {
-    pub fn alert(s: &str);
+pub struct RunResult {
+    out: String,
+    err: String,
+    compile_err: String,
 }
 
 #[wasm_bindgen]
-pub struct RunResult {
-    pub out: String,
-    pub err: String,
-    pub compile_err: String,
-}
-
 impl RunResult {
     pub fn new(out: String, err: String, compile_err: String) -> RunResult {
         RunResult {
@@ -22,27 +18,36 @@ impl RunResult {
             compile_err,
         }
     }
+
+    #[wasm_bindgen(getter)]
+    pub fn out(&self) -> String {
+        self.out.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn err(&self) -> String {
+        self.err.clone()
+    }
+
+    #[wasm_bindgen(getter)]
+    pub fn compile_err(&self) -> String {
+        self.compile_err.clone()
+    }
 }
 
 #[wasm_bindgen]
-pub fn run_zip_and_string(file: &str, source: &str) -> RunResult {
+pub fn run_zip_and_string(zip: Vec<u8>, source: &str) -> RunResult {
     let mut cfg = run_zip::Config::default();
     cfg.base_dir = Some("std/");
     let out_buf = WriteBuf::new();
     let err_buf = WriteBuf::new();
     cfg.std_out = Some(Box::new(out_buf.clone()));
     cfg.std_err = Some(Box::new(err_buf.clone()));
-    let zip = std::fs::read(std::path::Path::new(file)).unwrap();
     let result = run_zip::run_string(&zip, cfg, source);
     match result {
         Ok(_) => RunResult::new(out_buf.into_string(), err_buf.into_string(), "".to_owned()),
         Err(e) => RunResult::new("".to_owned(), "".to_owned(), format!("{}", e)),
     }
-}
-
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
 }
 
 #[derive(Clone)]
