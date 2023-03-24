2f21104304b4517ce339b0d9a2f145b43583a862
oxfeeefeee <pb01005051@gmail.com>
2023-03-24 18:37:04 +0800

    try fix config

--- next.config.js
+++ next.config.js
@@ -1,16 +1,8 @@
-
-const isProd = process.env.NODE_ENV === 'production'
+// next.config.js
 
 module.exports = {
-    webpack: (config, { isServer }) => {
-        // Enable WebAssembly as an experimental feature
-        config.experiments = { asyncWebAssembly: true };
-
-        return config;
-    },
-
-    assetPrefix: isProd ? '/goscript-dev-site/' : '',
     images: {
-        unoptimized: true,
+        loader: 'akamai',
+        path: '',
     },
 }
